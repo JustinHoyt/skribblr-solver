f4b97f7861e9214d64a9a40151d2112a0964918b
justinhoyt <justinhoyt@gmail.com>
2023-08-06 02:10:24 +0000

    Remove clipboard dependency from pl script

--- get_hints.pl
+++ get_hints.pl
@@ -1,13 +1,9 @@
 #!/usr/bin/env perl
 
-# Install dependencies with:
-# $ cpanm --installdeps .
-
-use feature ":5.30";
+use 5.30.0;
 use strict;
 use warnings;
 use autodie;
-use Clipboard;
 use Time::HiRes qw(sleep);
 
 open(my $fh, '<', 'dictionary.txt');
@@ -15,8 +11,7 @@ open(my $fh, '<', 'dictionary.txt');
 while(<$fh>) {
     chomp;
     if("@ARGV" eq "@{[map { length } split]}") {
-        say;
-        Clipboard->copy($_);
-        sleep(0.1);
+        `tty-copy $_`;
+        sleep(0.5);
     }
 }
