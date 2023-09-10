05e5a240c22e989ae048b2dcffb6aecb903e4dc8
justinhoyt <justinhoyt@gmail.com>
2023-09-10 19:50:00 +0000

    Add fzf searching to fish program

--- get_hints.fish
+++ get_hints.fish
@@ -1,3 +1,7 @@
 #!/usr/bin/env fish
 
-perl -nE "print if '$argv' eq qq(@{[map length, split]})" dictionary.txt
+perl -nE "print if '$argv' eq qq(@{[map length, split]})" dictionary.txt \
+    | fzf \
+    | perl -nE 'chomp; chomp; print' \
+    | tty-copy
+
